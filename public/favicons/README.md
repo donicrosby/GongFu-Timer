5bbb68aa05cd6b713c3a90f83bea8d514e252338
Stijn Ruts <github@stijnruts.be>
2017-05-08 21:11:51 +0200

    Improve favicon

---
+++ public/favicons/README.md
@@ -0,0 +1,2 @@
+Do not edit these files
+They are generated with 'npm run favicons'
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 import Helmet from 'react-helmet';
+import Favicons from './Favicons';
 import Overview from './Overview';
 import Edit from './Edit';
 import Timer from './Timer';
@@ -8,7 +9,8 @@ import Timer from './Timer';
 const App = () => (
   <BrowserRouter>
     <div>
-      <Helmet defaultTitle="GongFu Timer" />
+      <Helmet defaultTitle="GongFu Timer"/>
+      { Favicons }
 
       <Route exact path="/" component={Overview}/>
       <Route path="/edit" component={Edit}/>
